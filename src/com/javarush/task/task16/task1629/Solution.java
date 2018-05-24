4d410502ec280d2f2f400ed2a398296538ecf2c6
sadosdesperados <bms39rus@gmail.com>
2018-05-24 14:40:12 +0200

    Added task1629. The method printResult() don't a line counted by the stream

--- src/com/javarush/task/task16/task1629/Solution.java
+++ src/com/javarush/task/task16/task1629/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task16.task1629;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -12,10 +15,38 @@ public class Solution {
 
         //add your code here - добавьте код тут
 
+        /*t1.printResult();
+        t2.printResult();*/
+        t1.start();
         t1.printResult();
-        t2.printResult();
     }
 
     //add your code here - добавьте код тут
+    public static class Read3Strings extends Thread {
 
+        private static int c = 3;
+        private List<String> result = new ArrayList();
+
+        @Override
+        public void run() {
+            while (c>0) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                try {
+                    String str = reader.readLine();
+                    result.add(str);
+                    c--;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Thread.currentThread().interrupt();
+
+        }
+
+        public void printResult() {
+
+            System.out.println(result);
+
+        }
+    }
 }
