8dbb7f685e315e9d22d8b0933b2a9d977a0e62ed
sadosdesperados <bms39rus@gmail.com>
2018-06-05 13:38:50 +0200

    код работает, но потоки запускаются run() и join вызывается только у потока t1

--- src/com/javarush/task/task16/task1629/Solution.java
+++ src/com/javarush/task/task16/task1629/Solution.java
@@ -5,48 +5,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
-    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws InterruptedException {
+    private static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
-
-        /*t1.printResult();
-        t2.printResult();*/
-        t1.start();
+        t1.run();//t1 must using start();
+        t1.join();//this work without join();
+        t2.run();
         t1.printResult();
+        t2.printResult();
     }
 
     //add your code here - добавьте код тут
     public static class Read3Strings extends Thread {
 
-        private static int c = 3;
-        private List<String> result = new ArrayList();
+        private List<String> result = new ArrayList<String>();
+        private int c = 3;
 
         @Override
         public void run() {
-            while (c>0) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                try {
+            try {
+                while (c > 0) {
                     String str = reader.readLine();
                     result.add(str);
                     c--;
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                Thread.currentThread().interrupt();
+            } catch (IOException e) {
+                System.out.println("You have exception!");
+                e.printStackTrace();
             }
-            Thread.currentThread().interrupt();
-
-        }
+        }//the end of run();
 
         public void printResult() {
-
-            System.out.println(result);
-
+            for (String str:result) {
+                System.out.print(str + " ");
+            }
+            System.out.println();
         }
-    }
-}
+
+        @Override
+        public String toString() {
+            return result.toString();
+        }
+
+    }//the end of Read3Strings
+}//the end of Solution
