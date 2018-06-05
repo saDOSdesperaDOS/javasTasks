9e36cea8910afb78eb2099686b5b9d00a506753c
sadosdesperados <bms39rus@gmail.com>
2018-06-05 13:47:49 +0200

    added condition task1630

---
+++ src/com/javarush/task/task16/task1630/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1630;
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //add your code here - добавьте код тут
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        //add your code here - добавьте код тут
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    //add your code here - добавьте код тут
+}
