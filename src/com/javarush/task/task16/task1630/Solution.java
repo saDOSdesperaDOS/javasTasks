1b66df4e2000313af2b396e784865cf3841befed
sadosdesperados <bms39rus@gmail.com>
2018-07-27 18:11:30 +0200

    task1630 first requirement is fulfilled

--- src/com/javarush/task/task16/task1630/Solution.java
+++ src/com/javarush/task/task16/task1630/Solution.java
@@ -1,22 +1,32 @@
 package com.javarush.task.task16.task1630;
 
+import java.util.Scanner;
+
 public class Solution {
     public static String firstFileName;
     public static String secondFileName;
 
     //add your code here - добавьте код тут
+    static {
+        Scanner in = new Scanner(System.in);
+        firstFileName = in.nextLine();
+        secondFileName = in.nextLine();
+    }
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println(firstFileName);
+        System.out.println(secondFileName);
+
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
     }
 
     public static void systemOutPrintln(String fileName) throws InterruptedException {
-        ReadFileInterface f = new ReadFileThread();
+       /* ReadFileInterface f = new ReadFileThread();
         f.setFileName(fileName);
         f.start();
         //add your code here - добавьте код тут
-        System.out.println(f.getFileContent());
+        System.out.println(f.getFileContent());*/
     }
 
     public interface ReadFileInterface {
