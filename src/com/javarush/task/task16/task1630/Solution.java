4474af286188600b16884d3880674c71427c435b
sadosdesperados <bms39rus@gmail.com>
2018-08-03 19:51:20 +0200

    show only first line/выводит только первую строку файла

--- src/com/javarush/task/task16/task1630/Solution.java
+++ src/com/javarush/task/task16/task1630/Solution.java
@@ -1,6 +1,18 @@
 package com.javarush.task.task16.task1630;
 
-import java.util.Scanner;
+/*Требования:
++1.	Статический блок класса Solution должен считывать с консоли имена двух файлов и сохранять их в переменные firstFileName и secondFileName.
++2.	Объяви в классе Solution public static класс ReadFileThread.
++3.	Класс ReadFileThread должен реализовывать интерфейс ReadFileInterface.
++4.	Класс ReadFileThread должен быть унаследован от подходящего класса.
++5.	Метод run класса ReadFileThread должен считывать строки из файла, установленного методом setFileName. А метод getFileContent, этого же класса, должен возвращать вычитанный контент. Возвращаемое значение - это одна строка, состоящая из строк файла, разделенных пробелами.
+6+.	Метод systemOutPrintln должен вызывать метод join у созданного объекта f.
+7.	Вывод программы должен состоять из 2х строк. Каждая строка - содержимое одного файла.*/
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public static String firstFileName;
@@ -8,28 +20,31 @@ public class Solution {
 
     //add your code here - добавьте код тут
     static {
-        Scanner in = new Scanner(System.in);
-        firstFileName = in.nextLine();
-        secondFileName = in.nextLine();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(firstFileName);
-        System.out.println(secondFileName);
-
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
     }
 
     public static void systemOutPrintln(String fileName) throws InterruptedException {
-       /* ReadFileInterface f = new ReadFileThread();
+        ReadFileInterface f = new ReadFileThread();
         f.setFileName(fileName);
         f.start();
         //add your code here - добавьте код тут
-        System.out.println(f.getFileContent());*/
+        f.join();
+        System.out.println(f.getFileContent());
     }
 
-    public interface ReadFileInterface {
+    public interface ReadFileInterface  {
 
         void setFileName(String fullFileName);
 
@@ -41,4 +56,48 @@ public class Solution {
     }
 
     //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        String fullFileName;
+        List<String> content = new ArrayList<>();
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fullFileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            StringBuffer strContent = new StringBuffer();
+            for (String s:content) {
+                strContent.append(s).append(" ");
+            }
+            //return this.toString();
+            return strContent.toString();
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fullFileName)));
+                if (reader.ready()) {
+                    String str = reader.readLine();
+                    content.add(str);
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                    System.out.println("File "+fullFileName+" not found");
+            } catch (IOException e){
+                    System.out.println("File " + fullFileName + " can't read");
+            }
+        }
+    }
 }
+
+/*Требования:
++1.	Статический блок класса Solution должен считывать с консоли имена двух файлов и сохранять их в переменные firstFileName и secondFileName.
++2.	Объяви в классе Solution public static класс ReadFileThread.
++3.	Класс ReadFileThread должен реализовывать интерфейс ReadFileInterface.
++4.	Класс ReadFileThread должен быть унаследован от подходящего класса.
++5.	Метод run класса ReadFileThread должен считывать строки из файла, установленного методом setFileName. А метод getFileContent, этого же класса, должен возвращать вычитанный контент. Возвращаемое значение - это одна строка, состоящая из строк файла, разделенных пробелами.
+6.	Метод systemOutPrintln должен вызывать метод join у созданного объекта f.
+7.	Вывод программы должен состоять из 2х строк. Каждая строка - содержимое одного файла.*/
