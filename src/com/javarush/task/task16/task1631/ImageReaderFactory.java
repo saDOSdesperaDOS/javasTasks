f981c6d861702aff721832be293ec8f52201feab
sadosdesperados <bms39rus@gmail.com>
2018-08-17 11:11:10 +0200

    have decided

--- src/com/javarush/task/task16/task1631/ImageReaderFactory.java
+++ src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -1,13 +1,16 @@
 package com.javarush.task.task16.task1631;
 
-import com.javarush.task.task16.task1631.common.ImageReader;
-import com.javarush.task.task16.task1631.common.ImageTypes;
+import com.javarush.task.task16.task1631.common.*;
 
 public class ImageReaderFactory {
 
-    public static ImageReader getImageReader(ImageTypes imageType)  {
+    public static ImageReader getImageReader(ImageTypes imageType) throws IllegalArgumentException  {
         ImageReader imageReader=null;
-
+        switch (imageType) {
+            case JPG: imageReader = new JpgReader();
+            case BMP: imageReader = new BmpReader();
+            case PNG: imageReader = new PngReader();
+        }
         return imageReader;
     }
 }
--- src/com/javarush/task/task16/task1631/Solution.java
+++ src/com/javarush/task/task16/task1631/Solution.java
@@ -5,8 +5,8 @@ package com.javarush.task.task16.task1631;
        + 3.	Создай в пакете common класс BmpReader, реализующий интерфейс ImageReader.
        + 4.	Создай публичный класс ImageReaderFactory.
        +5.	Добавь в класс ImageReaderFactory открытый статический метод getImageReader с параметром ImageTypes.
-        6.	Метод getImageReader должен создавать и возвращать подходящий Reader.
-        7.	Метод getImageReader должен кидать исключение IllegalArgumentException, если передан неправильный параметр.*/
+        +6.	Метод getImageReader должен создавать и возвращать подходящий Reader.
+        +7.	Метод getImageReader должен кидать исключение IllegalArgumentException, если передан неправильный параметр.*/
 
 import com.javarush.task.task16.task1631.common.ImageReader;
 import com.javarush.task.task16.task1631.common.ImageTypes;
