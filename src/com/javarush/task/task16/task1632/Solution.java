9857a54c0b803cee821ae480129d33f6138d589d
sadosdesperados <bms39rus@gmail.com>
2018-09-16 14:04:47 +0200

    Task1633 solved. Replaced the method 'setUncaughtExceptionHandler(handler)' by 'setDefaultUncaughtExceptionHandler(handler)'

--- src/com/javarush/task/task16/task1632/Solution.java
+++ src/com/javarush/task/task16/task1632/Solution.java
@@ -6,9 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-
-import static java.lang.Thread.currentThread;
 
 /**
   @author: Mike Blokhin
--- src/com/javarush/task/task16/task1633/Solution.java
+++ src/com/javarush/task/task16/task1633/Solution.java
@@ -1,15 +1,27 @@
 package com.javarush.task.task16.task1633;
 
+
+/**
+ @author: Mike Blokhin
+ Требования:
+ V1.	Метод main должен создавать нить с параметрами: commonThread и "Нить 1".
+ V2.	Метод main должен создавать нить с параметрами: commonThread и "Нить 2".
+ V3.	Метод main должен запускать две созданные нити типа Thread.
+ V4.	Метод main должен прерывать две созданные нити типа Thread.
+ 5.	Программа с помощью метода uncaughtException класса OurUncaughtExceptionHandler должна вывести 2 сообщения.
+ 6.	Метод uncaughtException класса OurUncaughtExceptionHandler явно не вызывать.
+ 7.	Вывод программы должен содержать строки: "Нить 1: My exception message" и "Нить 2: My exception message".
+ **/
 public class Solution {
-    public static Thread.UncaughtExceptionHandler handler = new OurUncaughtExceptionHandler();
+    public static Thread.UncaughtExceptionHandler handler = new OurUncaughtExceptionHandler();//своя реализация метода  public void uncaughtException(Thread t, Throwable e)
 
     public static void main(String[] args) {
         TestedThread commonThread = new TestedThread(handler);
 
-        Thread threadA = new Thread(commonThread, "Нить 1");
+        Thread threadA = new Thread(commonThread, "Нить 1");//поток авт-и стартует при создании, т.к это указанно в кострукторе
         Thread threadB = new Thread(commonThread, "Нить 2");
 
-        threadA.start();
+        threadA.start();//поток засыпает на 3000 мл.сек, т.к это указанно в методе run()
         threadB.start();
 
         threadA.interrupt();
@@ -18,15 +30,18 @@ public class Solution {
 
     public static class TestedThread extends Thread {
         public TestedThread(UncaughtExceptionHandler handler) {
-            setUncaughtExceptionHandler(handler);
+            //setUncaughtExceptionHandler(handler);//установили оьработку неотлавлиемого исключения
+            setDefaultUncaughtExceptionHandler(handler);
             start();
         }
 
         public void run() {
             try {
                 Thread.sleep(3000);
+
             } catch (InterruptedException x) {
                 throw new RuntimeException("My exception message");
+
             }
         }
     }
@@ -34,7 +49,7 @@ public class Solution {
     public static class OurUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
         @Override
         public void uncaughtException(Thread t, Throwable e) {
-            System.out.println(t.getName() + ": " + e.getMessage());
+            System.out.println(t.getName() + ": " + e.getMessage());//переопределили метод uncaughtException(Thread t, Throwable e)
         }
     }
 }
